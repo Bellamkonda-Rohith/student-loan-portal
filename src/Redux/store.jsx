5d63002690a95a11d99eb919060e2d8139abd7d0
Bellamkonda Rohith <rohtihbellamkonda0@gmail.com>
2025-01-25 13:03:57 +0530

    solveed the log

--- src/Redux/store.jsx
+++ src/Redux/store.jsx
@@ -1,13 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import loanReducer from './loanSlice';
-import personalProfileSlice from './PersonalProfileSlice'
+import PersonalProfileSlice from './PersonalProfileSlice';
 import EducationalProfileSlice from './EducationalProfileSlice';
 import FinancialProfileSlice from './FinancialProfileSlice';
 import DashboardSlice from './DashboardSlice';
 export const store = configureStore({
   reducer: {
     loan: loanReducer,
-    Personalformdata: personalProfileSlice,
+    Personalformdata: PersonalProfileSlice,
     Educationalform: EducationalProfileSlice,
     FinancialProfileform: FinancialProfileSlice,
     DashboardDetails: DashboardSlice,
